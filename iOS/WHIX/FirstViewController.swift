8f673033d57e4af3debf7f4d3a605a5e6d5843fb
Alexandru Rosianu <me@aluxian.com>
2019-02-23 13:43:34 +0000

    hello world of ioS

---
+++ iOS/WHIX/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WHIX
+//
+//  Created by Alexandru Rosianu on 23/02/2019.
+//  Copyright © 2019 WHIX. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS/WHIX/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WHIX
+//
+//  Created by Alexandru Rosianu on 23/02/2019.
+//  Copyright © 2019 WHIX. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
